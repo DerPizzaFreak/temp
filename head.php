c7482201de5d45826beed0db07d0c2c1ddc4207e
Julian <102260706+DerPizzaFreak@users.noreply.github.com>
2023-10-24 17:18:53 +0200

    head.php

--- head.php
+++ head.php
@@ -1,66 +1,41 @@
 <?php
 session_start(); // Session starten
 
-function success($usrname, $pw)
-{
-	return '<!DOCTYPE html>
-			   <html>
-				   <head>
-					   <title>Reg/Log</title>
-				   </head>
-				   <body>
-					   <h1 style="color: green;">Ihr Nutzername:'.$username.'<br>Ihr Passwort:'.$password.'</h1>
-					   <a href="index.html">Back</a>
-				   </body>
-			   </html>
-	   ';
-}
-
-function error($fehler)
-{
-	return '<!DOCTYPE html>
-			   <html>
-				   <head>
-					   <title>Reg/Log</title>
-				   </head>
-				   <body>
-					   <h1 style="color: red;">'.$fehler.'</h1>
-					   <a href="index.html">Back</a>
-				   </body>
-			   </html>
-	   ';
-}
-
 // Überprüfen, ob ein Benutzername und ein Passwort gesendet wurden
-if (isset($_POST['username']) && isset($_POST['password1'])) {
+if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    // Hier sollten Sie eine Datenbankverbindung herstellen und die Benutzerdaten überprüfen.
+    // Dieser Teil hängt von Ihrer tatsächlichen Implementierung ab.
+
     if (isset($_POST['login'])) {
+        // Implementieren Sie hier den Login-Prozess
+        // Überprüfen Sie, ob der Benutzer existiert und das Passwort korrekt ist.
+        // Sie können eine Datenbankabfrage verwenden.
 
-		// Beispiel Account
-        if ($username == 'Test' && $password == 'Passwort') {
-			echo(success($username, $password));
+        // Beispiel:
+        if ($username == 'demo' && $password == 'pass') {
+            $_SESSION['username'] = $username; // Benutzer in der Sitzung speichern
+            header('Location: welcome.php'); // Weiterleitung zur Willkommensseite
+            exit();
         } else {
-			die(error('Benutzer nicht vorhanden oder falsches Passwort!'));
+            // Login fehlgeschlagen
+            header('Location: index.html'); // Zurück zum Login-Formular
+            exit();
         }
+    } elseif (isset($_POST['register'])) {
+        // Implementieren Sie hier den Registrierungsprozess
+        // Fügen Sie den Benutzer in die Datenbank ein.
 
+        // Beispiel:
+        // Hier sollten Sie die Überprüfung der Benutzerdaten und die Datenbankabfrage einfügen.
+        // Wenn die Registrierung erfolgreich ist, können Sie den Benutzer in die Datenbank einfügen.
+        // Danach leiten Sie den Benutzer zur Login-Seite weiter.
     }
-	elseif (isset($_POST['register']))
-	{
-        // kommt noch
-    }
-}
-else
-{
+} else {
     // Ungültige Anfrage, zurück zum Login-Formular
-    if (!isset($_POST['username']))
-	{
-		die(error('Der Benutzername wurde nicht eingegeben!'));
-	}
-	 elseif (!isset($_POST['password1']))
-	 {
-		die(error('Das Passwort wurde nicht eingegeben!'));
-	}
+    header('Location: index.html');
+    exit();
 }
 ?>
