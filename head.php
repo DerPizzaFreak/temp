4e9d135ee66a9f81a44bce1047386e70a29aeace
Julian <102260706+DerPizzaFreak@users.noreply.github.com>
2023-10-24 17:32:38 +0200

    head.php

--- head.php
+++ head.php
@@ -17,11 +17,29 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         // Beispiel:
         if ($username == 'demo' && $password == 'pass') {
             $_SESSION['username'] = $username; // Benutzer in der Sitzung speichern
-            header('Location: index2.htm'); // Weiterleitung zur Willkommensseite
+            echo('<!DOCTYPE html>
+			   <html>
+				   <head>
+					   <title>Reg/Log</title>
+				   </head>
+				   <body>
+					   <h1 style="color: green;">success</h1>
+					   <a href="index.html">Back</a>
+				   </body>
+			   </html>');
             exit();
         } else {
             // Login fehlgeschlagen
-            header('Location: index.html'); // Zurück zum Login-Formular
+            echo('<!DOCTYPE html>
+			   <html>
+				   <head>
+					   <title>Reg/Log</title>
+				   </head>
+				   <body>
+					   <h1 style="color: red;">fail</h1>
+					   <a href="index.html">Back</a>
+				   </body>
+			   </html>');
             exit();
         }
     } elseif (isset($_POST['register'])) {
@@ -35,7 +53,17 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     }
 } else {
     // Ungültige Anfrage, zurück zum Login-Formular
-    header('Location: index.html');
-    exit();
+    
+            echo('<!DOCTYPE html>
+			   <html>
+				   <head>
+					   <title>Reg/Log</title>
+				   </head>
+				   <body>
+					   <h1 style="color: red;">fail 2</h1>
+					   <a href="index.html">Back</a>
+				   </body>
+			   </html>');
+            exit();
 }
 ?>
